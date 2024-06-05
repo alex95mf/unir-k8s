098fd478516f159fa5ef2fed96213af2d9daf93d
Alexander Martinez <alexander.martinez@twiinshrm.com>
2024-06-05 15:47:11 -0500

    [NEW] Inicialización de proyecto de actividad grupal

---
+++ nodejs-mongodb-app/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./db');
+
+const app = express();
+const PORT = 3000;
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
